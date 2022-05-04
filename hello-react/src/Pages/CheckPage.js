a4d17b7766efbd8ddfed53fee9ac826d811d4c39
donghoon_han <donghoon_han@tmax.co.kr>
2022-05-04 18:04:16 +0900

    no message

--- hello-react/src/Pages/CheckPage.js
+++ hello-react/src/Pages/CheckPage.js
@@ -2,3 +2,22 @@
 // 밤 12시가 되면 init이 되도록
 // 그 외의 경우 state함수나 무엇인가를 통해 대기
 // db가 제일 적절하다 생각함
+
+
+import React, { useState,useEffect } from 'react';
+import { Outlet,useLocation, useNavigate } from 'react-router-dom';
+
+const MainView = () => {
+
+    const [isCheckTableOpen, setCheckTableOpen] = useState(false);
+    // 
+
+    return (
+        <div className="Main">
+            <TopBar onCheckPage={() => setCheckTableOpen(true)}/>
+            <div className={classes.content} >
+                <Outlet />
+            </div>
+        </div>
+    )
+}
