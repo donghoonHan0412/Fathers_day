ae68cf3bb19a51b2a40c291092185c4df4b758ff
donghoon_han <donghoon_han@tmax.co.kr>
2022-05-06 00:53:52 +0900

    "0505 시작"

---
+++ hello-react/src/Pages/main.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+const MainLayout = () => {
+
+    return (
+        <div>
+            "it is MainLayouts"
+        </div>
+    )
+}
+
+
+export default MainLayout;
